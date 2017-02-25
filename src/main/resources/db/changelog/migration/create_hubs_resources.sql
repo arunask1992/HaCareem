fa2072ae343682e3d38f3e19e778c95474434f3c
Aruna Shanmugakumar <arunash@thoughtworks.com>
2017-02-25 16:52:13 +0530

    [Aruna] Added design artifacts/RabbitMQ/seed data

---
+++ src/main/resources/db/changelog/migration/create_hubs_resources.sql
@@ -0,0 +1,5 @@
+INSERT INTO hubs (name, position) values ('Velachery', '{"latitude" : "12.975971", "longitude" : "80.2212092"}');
+INSERT INTO hubs (name, position) values ('Thiruvanmiyur', '{"latitude" : "12.9830269", "longitude" : "80.2594001"}');
+INSERT INTO hubs (name, position) values ('Adyar', '{"latitude" : "13.0011774", "longitude" : "80.2564957"}');
+INSERT INTO hubs (name, position) values ('Medavakkam', '{"latitude" : "12.9171427", "longitude" : "80.1923489"}');
+INSERT INTO hubs (name, position) values ('Tambaram', '{"latitude" : "12.9229153", "longitude" : "80.1274558"}');
